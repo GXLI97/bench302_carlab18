cf9aeaaa2c331f0e4bf14d5378f1ec7410bbaf57
GXLI97 <gxli@princeton.edu>
2018-05-03 17:52:06 -0400

    encoder

--- drivestraight.py
+++ drivestraight.py
@@ -9,7 +9,7 @@ Renc = a_star.read_encoders()[1]
 start = time.time()
 end = start + 10
 
-Kp = .0001
+Kp = .01
 L, R = 100, 100
 a_star.motors(L, R)
 
@@ -19,10 +19,12 @@ while 1:
         a_star.motors(0, 0)
         break
     E = a_star.read_encoders()
-    err = E[0] - E[1]
+    err = (E[0] - Lenc) - (E[1] - Renc)
+    Lenc = E[0]
+    Renc = E[1]
     err = Kp*err
     print("{:.2f}".format(err))
-    L += err
-    R -= err
+    L -= err
+    R += err
     a_star.motors(int(L), int(R))
-    time.sleep(0.05)
+    time.sleep(0.1)
