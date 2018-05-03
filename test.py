718ca30f42a0cf608b425dcb1df301042e9d01a5
GXLI97 <gxli@princeton.edu>
2018-05-03 17:16:49 -0400

    encoder

--- test.py
+++ test.py
@@ -3,6 +3,9 @@ import time
 
 a_star = AStar()
 
+L = a_star.read_encoders()[0]
+R = a_star.read_encoders()[1]
+
 a_star.motors(25, -25)
 
 start = time.time()
@@ -12,5 +15,5 @@ while 1:
     if time.time()>end:
         a_star.motors(0, 0)
         break
-    print(a_star.read_encoders())
+    print(a_star.read_encoders()[0] - L, a_star.read_encoders()[1]-R)
     time.sleep(0.1)
