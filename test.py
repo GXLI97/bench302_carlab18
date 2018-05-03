89382981bbdaaa578568abc8bb814ed64ced4233
GXLI97 <gxli@princeton.edu>
2018-05-03 17:28:16 -0400

    encoder

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ print(Eopt)
 while 1:
     Eopt = calcEncodersRight()
     E = a_star.read_encoders()
-    if (E[0]-L > Eopt[0] and E[1]-R > Eopt[1]):
+    if (E[0]-L > Eopt[0] or E[1]-R > Eopt[1]):
         break
     print(E)
     time.sleep(0.05)
