f9e0504d11d0ab378203f51549aacaf4b2b6bd86
gnmelnick <gmelnick3435@gmail.com>
2018-05-04 11:37:39 -0400

    turn.py

--- turn.py
+++ turn.py
@@ -16,7 +16,7 @@ def turn(a_star, degrees):
     l = a_star.read_encoders()[0]
     r = a_star.read_encoders()[1]
 
-    a_star.motors(25, -25)
+    a_star.motors(100, -100)
     while 1:
         e = a_star.read_encoders()
         if abs(e[1]-r) + abs(e[0]-l) > 2 * ticks:
