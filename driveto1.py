6dee6ca2e5468103a045867bae9b2321d829d0b9
GXLI97 <gxli@princeton.edu>
2018-05-04 15:39:24 -0400

    changes

--- driveto1.py
+++ driveto1.py
@@ -54,6 +54,22 @@ def record_distance(ser):
             continue
     return mean(distances)
 
+def calc_angle(dist, d1, d2, d3):
+    cosT1 = (dist**2 + d2**2 - d1**2)/(2*dist*d2)
+    cosT2 = (dist**2 + d2**2 - d3**2)/(2*dist*d2)
+    T1 = math.acos(cosT1)
+    T2 = math.acos(cosT2)
+    if cosT1 > 0 and cosT2 > 0:
+        angle = mean([math.pi/2-T1, T2])
+    elif cosT1 > 0 and cosT2 < 0:
+        angle = mean([math.pi/2+T1, T2])
+    elif cosT1 < 0 and cosT2 > 0:
+        angle = mean([math.pi/2-T1, -T2])
+    elif cosT1 < 0 and cosT2 < 0:
+        angle = mean([T1-3*math.pi/2,-T2])
+    return angle * 180/math.pi
+
+
 def shutdown(ser, a_star):
     ser.write(b'lec\r')
     ser.close()
@@ -67,15 +83,27 @@ def main():
 
     ser = connect_to_serial()
 
+    EVAL_DIST
+
     d1 = record_distance(ser)
-    drive_straight(a_star, dist=2)
+    drive_straight(a_star, dist=EVAL_DIST)
     d2 = record_distance(ser)
     turn(a_star, degrees=90, clockwise=1)
     time.sleep(0.5)
-    drive_straight(a_star, dist=2)
+    drive_straight(a_star, dist=EVAL_DIST)
     d3 = record_distance(ser)
 
     print("Distances {:.2f} {:.2f} {:.2f}".format(d1, d2, d3))
+
+    # go now to correct location:
+    drive_straight(a_star, dist=2, forward=-1)
+    degrees = calc_angle(EVAL_DIST, d1, d2, d3)
+    if degrees > 0:
+        turn(a_star, degrees, clockwise=1)
+    else:
+        turn(a_star, degrees, clockwise=-1)
+
+
     shutdown(ser, a_star)
 
 if __name__ == '__main__':
