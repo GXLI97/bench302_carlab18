62f5693898e025d230028a826647a1b09edad657
gnmelnick <gmelnick3435@gmail.com>
2018-05-04 13:20:29 -0400

    turn.py

--- turn.py
+++ turn.py
@@ -49,7 +49,7 @@ def main():
         turn(a_star, 90)
 
     else:
-        turn(a_star, float(sys.argv[1]), 1, float(sys.argv[2]), float(sys.argv[3]))
+        turn(a_star, float(sys.argv[1]), -1, float(sys.argv[2]), float(sys.argv[3]))
 
 if __name__ == '__main__':
     main()
