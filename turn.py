4147760ad8f5cb475385c198f66868f21ed9b031
GXLI97 <gxli@princeton.edu>
2018-05-05 12:48:27 -0400

    changes

--- turn.py
+++ turn.py
@@ -5,8 +5,8 @@ import sys
 
 
 
-def turn(a_star, degrees, clockwise=1, Kp=1, Ki=.08):
-    BOTDIAM = 145.
+def turn(a_star, degrees, clockwise=1, Kp=1.5, Ki=.05):
+    BOTDIAM = 149.
     WHEELDIAM = 70.
     ENCODERTICKS = 1440.
     OVERFLOW_BUFF = 65536
