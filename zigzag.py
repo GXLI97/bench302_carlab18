ab3c60209ee012e69e20ed1f75aec3d1e1ff56e7
GXLI97 <gxli@princeton.edu>
2018-05-05 13:42:58 -0400

    changes

--- zigzag.py
+++ zigzag.py
@@ -54,56 +54,10 @@ def record_distance(ser):
             continue
     return median(distances)
 
-def calc_angle(dist, d1, d2, d3):
-    cosT1 = (dist**2 + d2**2 - d1**2)/(2*dist*d2)
-    if cosT1 > 1:
-        cosT1 = 1
-    elif cosT1 < -1:
-        cosT1 = -1
-    cosT2 = (dist**2 + d2**2 - d3**2)/(2*dist*d2)
-    if cosT2 > 1:
-        cosT2 = 1
-    elif cosT2 < -1:
-        cosT2 = -1
-    T1 = math.acos(cosT1)
-    T2 = math.acos(cosT2)
-    if cosT1 > 0 and cosT2 > 0:
-        angle = mean([math.pi/2-T1, T2])
-    elif cosT1 > 0 and cosT2 < 0:
-        angle = mean([math.pi/2+T1, T2])
-    elif cosT1 < 0 and cosT2 > 0:
-        angle = mean([math.pi/2-T1, -T2])
-    elif cosT1 < 0 and cosT2 < 0:
-        angle = mean([T1-3*math.pi/2,-T2])
-    return angle * 180/math.pi
-
-
 def shutdown(ser, a_star):
     ser.write(b'lec\r')
     ser.close()
     a_star.motors(0, 0)
-
-def move_step(ser, a_star, EVAL_DIST):
-    d1 = record_distance(ser)
-    drive_straight(a_star, dist=EVAL_DIST)
-    d2 = record_distance(ser)
-    turn(a_star, degrees=90, clockwise=1)
-    time.sleep(0.5)
-    drive_straight(a_star, dist=EVAL_DIST)
-    d3 = record_distance(ser)
-
-    print("Distances {:.2f} {:.2f} {:.2f}".format(d1, d2, d3))
-
-    # go now to correct location:
-    drive_straight(a_star, dist=EVAL_DIST, forward=-1)
-    degrees = calc_angle(EVAL_DIST, d1, d2, d3)
-    print("Calculated turn angle: {:.2f}".format(degrees))
-    time.sleep(0.5)
-    turn(a_star, degrees, clockwise=1)
-    time.sleep(0.5)
-    drive_straight(a_star, dist=d2 * 0.4)
-    dend = record_distance(ser)
-    return dend
 
 def zigzag(a_star, stride=1):
     time.sleep(0.5)
