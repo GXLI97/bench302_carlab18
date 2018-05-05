f2994f1602aeb1f1f0202735afe4d83a1dca1758
GXLI97 <gxli@princeton.edu>
2018-05-05 15:26:21 -0400

    changes

--- zigzag.py
+++ zigzag.py
@@ -112,6 +112,7 @@ def zag(ser, a_star):
         print("angle: {:.2f}".format(angle))
         turn(a_star, angle)
         time.sleep(0.5)
+        break
         drive_straight(a_star, d4*0.25)
 
 def main():
