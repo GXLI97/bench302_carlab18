0116d9e5cd7de5eea5674ae533ced46f7da9210f
GXLI97 <gxli@princeton.edu>
2018-05-05 20:43:40 -0400

    changes

--- zigzag.py
+++ zigzag.py
@@ -94,7 +94,7 @@ def zigzag(ser, a_star, stride):
 def calc_angle(di, dr, dl, df):
     x = di-df
     y = dl - dr
-    return math.degrees(math.atan2(y, x)) + 30
+    return math.degrees(math.atan2(y, x))
 
 def zag(ser, a_star):
     # do stuff.
