181994b51979a47c7f55c4df37d779f9ba297b27
gnmelnick <gmelnick3435@gmail.com>
2018-05-06 15:21:21 -0400

    create shutdown.py

---
+++ shutdown.py
@@ -0,0 +1,60 @@
+import serial
+import time
+import sys
+from a_star import AStar
+
+
+def connect_to_serial():
+    try:
+        ser = serial.Serial(
+        port='/dev/serial0',
+        baudrate=115200,
+        timeout=0.5
+        )
+        print("connected successfully!")
+    except:
+        ser = serial.Serial(
+        port='/dev/ttyACM1',
+        baudrate=115200,
+        timeout=0.5
+        )
+
+    time.sleep(1)
+    ser.write(b'\r\r') # go into serial mode.
+    time.sleep(2)
+    res=ser.read(100) # read some things.
+    time.sleep(0.5)
+    time.sleep(0.5)
+    ser.write(b'lec\r') # start writing distances.
+    return ser
+
+
+def shutdown(ser, a_star):
+    ser.write(b'lec\r')
+    ser.close()
+    a_star.motors(0, 0)
+
+
+def main:
+    a_star = AStar()
+    try:
+        ser = serial.Serial(
+        port='/dev/serial0',
+        baudrate=115200,
+        timeout=0.5
+        )
+        print("connected successfully!")
+    except:
+        ser = serial.Serial(
+        port='/dev/ttyACM1',
+        baudrate=115200,
+        timeout=0.5
+        )
+    time.sleep(1)
+    ser.write(b'\r\r') # go into serial mode.
+    shutdown(ser, a_star)
+
+
+
+if __name__ == '__main__':
+    main()
