44f7d1fb8734f820a5053be15f67d15b745325fc
GXLI97 <gxli@princeton.edu>
2018-05-06 22:47:34 -0400

    changes

--- wiggle.py
+++ wiggle.py
@@ -6,12 +6,21 @@ import sys
 
 a_star = AStar()
 
-a_star.motors(100, 150)
-time.sleep(2)
-a_star.motors(180, 100)
-time.sleep(2)
-a_star.motors(100, 150)
-time.sleep(2)
-a_star.motors(180, 100)
-time.sleep(2)
-a_star.motors(0, 0)
+def wiggle(forward = 1):
+    a_star.motors(100, 150)
+    time.sleep(2)
+    a_star.motors(180, 100)
+    time.sleep(2)
+    a_star.motors(100, 150)
+    time.sleep(2)
+    a_star.motors(180, 100)
+    time.sleep(2)
+    a_star.motors(0, 0)
+
+def main():
+    a_star = AStar()
+    wiggle(sys.argv[1])
+
+if __name__ == '__main__':
+    main()
+
