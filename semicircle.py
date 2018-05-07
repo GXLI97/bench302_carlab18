6edb00e8c5e61b427d3be785585df36be40d3a3a
gnmelnick <gmelnick3435@gmail.com>
2018-05-07 14:24:44 -0400

    semicircle

--- semicircle.py
+++ semicircle.py
@@ -26,7 +26,7 @@ def semicircle(a_star, radius, rightTurn=1, Ki=.04, Kp=1.3):
         # get encoder reading
         (Lcurr, Rcurr) = a_star.read_encoders()
 
-        if (Lcurr > Lfinal and Rcurr > Rfinal):
+        if (Lcurr > Lfinal or Rcurr > Rfinal):
             print("{} {} {} {}".format(Lcurr, Lfinal, Rcurr, Rfinal))
             a_star.motors(0, 0)
             break
