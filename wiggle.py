f92d695eee98bb59660ccb6e29f2f1c19d405200
GXLI97 <gxli@princeton.edu>
2018-05-06 22:49:11 -0400

    changes

--- wiggle.py
+++ wiggle.py
@@ -7,13 +7,15 @@ import sys
 a_star = AStar()
 
 def wiggle(forward = 1):
-    a_star.motors(100, 150)
+    L1, R1 = 100*forward, 150*forward
+    a_star.motors(L1, R1)
     time.sleep(2)
-    a_star.motors(180, 100)
+    L2, R2 = 180*forward, 100*forward
+    a_star.motors(L2, R2)
     time.sleep(2)
-    a_star.motors(100, 150)
+    a_star.motors(L1, R1)
     time.sleep(2)
-    a_star.motors(180, 100)
+    a_star.motors(L2, R2)
     time.sleep(2)
     a_star.motors(0, 0)
 
