d310536989679468760164a663fa7fc7440e156d
GXLI97 <gxli@princeton.edu>
2018-05-09 15:03:42 -0400

    changes to continuous drive

--- socket_server.py
+++ socket_server.py
@@ -1,7 +1,7 @@
 import socket
 import time
 
-host = '10.9.229.181'
+host = '10.9.67.44'
 port = 50008
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,8 +13,8 @@ while True:
     data = conn.recv(1024)
     if not data: break
     print("Received: "+(data.decode()))
-    response = input("Reply: ")
-    if response == "exit":
-        break
-    conn.sendall(response.encode())
+    # response = input("Reply: ")
+    # if response == "exit":
+    #     break
+    # conn.sendall(response.encode())
 conn.close()
