2dcd2ba029f12ad5fd3edbdd4b0f1b91a89e0376
gnmelnick <gmelnick3435@gmail.com>
2018-05-08 21:34:21 -0400

    try inplace turns

--- continuousdrive.py
+++ continuousdrive.py
@@ -130,8 +130,8 @@ def meander(a_star, q):
     OFFSET = 0
 
     while 1:
-        arcdrive(a_star, radius=0.125, arc=larc, speed=1.5)
-        arcdrive(a_star, radius=0.125, arc=rarc, speed=1.5, leftTurn=-1)
+        arcdrive(a_star, radius=0.25, arc=larc, speed=1.5)
+        arcdrive(a_star, radius=0.25, arc=rarc, speed=1.5, leftTurn=-1)
 
         dist_data = []
         while not q.empty():
