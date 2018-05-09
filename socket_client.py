4a1a6f5a96a665bf2c874c6165a963800dc9bf7e
gnmelnick <gmelnick3435@gmail.com>
2018-05-09 14:56:23 -0400

    socket_client

--- socket_client.py
+++ socket_client.py
@@ -5,13 +5,13 @@ port = 50008
  
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
-print("Connected to "+(host)+" on port "+str(port))
+print("Connected to {} on port {}".format(host, port))
 initialMessage = input("Send: ")
 s.sendall(initialMessage.encode('utf-8'))
  
 while True:
 	data = s.recv(1024)
-	print("Received: "+(data.decode()))
+	print("Received: {}".format(data.decode()))
 	response = input("Reply: ")
 	if response == "exit":
 		break
