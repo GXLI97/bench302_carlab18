afd68bfaff0802e846c6ac062785dd6a17423e54
gnmelnick <gmelnick3435@gmail.com>
2018-05-09 14:49:25 -0400

    socket_client

--- socket_client.py
+++ socket_client.py
@@ -6,13 +6,13 @@ port = 50008
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 print("Connected to "+(host)+" on port "+str(port))
-initialMessage = raw_input("Send: ")
+initialMessage = input("Send: ")
 s.sendall(initialMessage)
  
 while True:
 	data = s.recv(1024)
 	print("Recieved: "+(data))
-	response = raw_input("Reply: ")
+	response = input("Reply: ")
 	if response == "exit":
 		break
 	s.sendall(response)
