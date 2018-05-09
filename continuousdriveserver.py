f8d56f115a443e3cc6f237e395aad55086565eae
gnmelnick <gmelnick3435@gmail.com>
2018-05-09 15:22:22 -0400

    sockets in continuous drive

--- continuousdriveserver.py
+++ continuousdriveserver.py
@@ -13,64 +13,17 @@ import numpy as np
 import socket
 # from arcdrive import arcdrive
 
-def connect_to_serial():
-    try:
-        ser = serial.Serial(
-        port='/dev/serial0',
-        baudrate=115200,
-        timeout=0.5
-        )
-        print("connected successfully!")
-    except:
-        ser = serial.Serial(
-        port='/dev/ttyACM1',
-        baudrate=115200,
-        timeout=0.5
-        )
-
-    time.sleep(1)
-    ser.write(b'\r\r') # go into serial mode.
-    time.sleep(2)
-    res=ser.read(100) # read some things.
-    time.sleep(0.5)
-    time.sleep(0.5)
-    ser.write(b'lec\r') # start writing distances.
-    return ser
-
-def parseDistance(s, ID="0C25"):
-
-    # DIST,2,AN0,820C,0.00,0.00,0.00,7.24,AN1,0C25,0.00,0.00,0.00,2.55
-    a = s.strip().split(',')
-    k = 0
-    while not a[-6*k -5] == ID:
-        k += 1
-    # print(a)
-    return float(a[-6*k -1])
-
-def read_distances(ser, q, s):
-    i = 0
-    while 1:
-        try:
-            res = ser.readline()
-            dist = parseDistance(res.decode('utf-8'))
-            if not s is None:
-                s.sendall(str(dist).encode('utf-8'))
-            # print("Distance: {:.2f}".format(dist))
-            q.put_nowait(dist)
-            i += 1
-        except:
-            print("Read'n Parse failed")
-            continue
+def read_distances(q, s):
+    while True:
+        data = conn.recv(1024)
+        q.put_nowait(float(data.decode()))
     
 
 
-def shutdown(ser, a_star, p, s):
-    ser.write(b'lec\r')
-    ser.close()
+def shutdown(a_star, p, conn):
     p.terminate()
     a_star.motors(0, 0)
-    if not s is None:
-        s.close()
+    conn.close()
 
 def arcdrive(a_star, radius, leftTurn=1, arc=180, speed=1.5):
     BOTDIAM = 149.
@@ -218,24 +171,22 @@ def main():
 
     a_star = AStar()
 
-    ser = connect_to_serial()
-
-
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, port))
-
-
+    s.listen(1)
+    conn, addr = s.accept()
+    print ("Connection from", addr)
 
     q = Queue()
-    p = Process(target=read_distances, args=(ser, q, s))
+    p = Process(target=read_distances, args=(q, s))
     p.start()
 
     try:
         meander(a_star, q)
-        shutdown(ser, a_star, p, s)
+        shutdown(ser, a_star, p, conn)
     except (ErrorNumber, ErrorMessage):
         print(ErrorMessage)
-        shutdown(ser, a_star, p, s)
+        shutdown(ser, a_star, p, conn)
 
 
 
