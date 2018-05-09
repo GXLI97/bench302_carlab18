93f2e553fba2e16cb0076010ba38bb4d4ee3ed3c
GXLI97 <gxli@princeton.edu>
2018-05-09 14:59:49 -0400

    changes to continuous drive

--- socket_client.py
+++ socket_client.py
@@ -6,14 +6,14 @@ port = 50008
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 print("Connected to {} on port {}".format(host, port))
-initialMessage = input("Send: ")
-s.sendall(initialMessage.encode('utf-8'))
+# initialMessage = input("Send: ")
+# s.sendall(initialMessage.encode('utf-8'))
  
 while True:
 	data = s.recv(1024)
-	print("Received: {}".format(data.decode()))
-	response = input("Reply: ")
-	if response == "exit":
-		break
-	s.sendall(response.encode('utf-8'))
+	print("Received: {}".format(int(data.decode()))
+	# response = input("Reply: ")
+	# if response == "exit":
+	# 	break
+	# s.sendall(response.encode('utf-8'))
 s.close()
