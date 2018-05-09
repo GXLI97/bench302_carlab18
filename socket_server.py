702bc0f4f42f280ed00e0421744d164d7fc001d7
GXLI97 <gxli@princeton.edu>
2018-05-09 14:45:02 -0400

    changes to continuous drive

---
+++ socket_server.py
@@ -0,0 +1,20 @@
+import socket
+import time
+
+host = '10.9.229.181'
+port = 50008
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
+s.listen(1)
+conn, addr = s.accept()
+print ("Connection from", addr)
+while True:
+    data = conn.recv(1024)
+    if not data: break
+    print("Recieved: "+(data))
+    response = raw_input("Reply: ")
+    if response == "exit":
+        break
+    conn.sendall(response)
+conn.close()
