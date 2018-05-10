19f320dbaf45d7ed18408ed7d28e743f2f0733e4
GXLI97 <gxli@princeton.edu>
2018-05-09 21:24:10 -0400

    changes to continuous drive

--- continuousdriveserver.py
+++ continuousdriveserver.py
@@ -196,6 +196,7 @@ def main():
     a_star = AStar()
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((host, port))
     s.listen(1)
     conn, addr = s.accept()
